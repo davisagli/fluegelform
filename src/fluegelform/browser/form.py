730fa65ba9b7f292a203c8322dd7b620f0ba43c8
David Glick <dglick@gmail.com>
2011-09-09 21:49:51 -0700

    finish cleanup

--- src/fluegelform/browser/form.py
+++ src/fluegelform/browser/form.py
@@ -1,9 +1,10 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope.cachedescriptors.property import Lazy as lazy_property
+from z3c.form import form
 from plone.autoform.form import AutoExtensibleForm
 
 
-class FormView(AutoExtensibleForm):
+class FormView(AutoExtensibleForm, form.Form):
     template = ViewPageTemplateFile('templates/form_view.pt')
 
     @lazy_property
@@ -18,15 +19,5 @@ class FormView(AutoExtensibleForm):
     def schema(self):
         return self.context.schema
 
+    ignoreContext = True
     additionalSchemata = ()
-
-    def __init__(self, context, request):
-        # Override to avoid hiding the green border
-        super(FormView, self).__init__(context, request)
-    
-    def updateActions(self):
-        # override to re-title save button and remove the cancel button
-        super(FormView, self).updateActions()
-        self.buttons['save'].title = self.context.submit_label
-        if 'cancel' in self.buttons:
-            del self.buttons['cancel']
